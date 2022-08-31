a593f63341e757d5a8b930ecf80716afba7946e4
andrey <andrey.a.pushkin@gmail.com>
2022-08-31 06:39:26 -0400

    add

--- main_rss.py
+++ main_rss.py
@@ -5,19 +5,31 @@ from datetime import timedelta, datetime
 import feedparser
 import requests
 
-BAD_COUNTRIES = os.environ["BAD_COUNTRIES"].split(",")
-TIME_IN_PAST = timedelta(minutes=int(os.environ["TIME_IN_PAST"]))
-MIN_BUDGET = int(os.environ["MIN_BUDGET"])
-MIN_RANGE_FROM = int(os.environ["MIN_RANGE_VALUE"])
-MAX_RANGE_FROM = int(os.environ["MAX_RANGE_VALUE"])
-#SEARCH_QUERIES = os.environ["SEARCH_QUERIES"].split(",")
+# BAD_COUNTRIES = os.environ["BAD_COUNTRIES"].split(",")
+# TIME_IN_PAST = timedelta(minutes=int(os.environ["TIME_IN_PAST"]))
+# MIN_BUDGET = int(os.environ["MIN_BUDGET"])
+# MIN_RANGE_FROM = int(os.environ["MIN_RANGE_VALUE"])
+# MAX_RANGE_FROM = int(os.environ["MAX_RANGE_VALUE"])
+# #SEARCH_QUERIES = os.environ["SEARCH_QUERIES"].split(",")
+# SEARCH_QUERIES = ["data engineer", "airflow"]
+
+TELEGRAM_TOKEN='5531011071:AAGWFFKwFZF0LI4KIA_81GApPzguubXr3ZY'
+TELEGRAM_CHAT_ID=[5228302997, 406962410]
+BAD_COUNTRIES=['India']
+TIME_IN_PAST=timedelta(minutes=5)
+MIN_BUDGET=100
+MIN_RANGE_VALUE=15
+MAX_RANGE_VALUE=35
 SEARCH_QUERIES = ["data engineer", "airflow"]
 
 def send_msg(text):
-    token = os.environ["TELEGRAM_TOKEN"]
-    chat_id = os.environ["TELEGRAM_CHAT_ID"]
-    url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
-    requests.get(url_req)
+    token = TELEGRAM_TOKEN
+    chat_id = TELEGRAM_CHAT_ID
+
+    for _id in chat_id:
+
+        url_req = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={_id}&text={text}"
+        requests.get(url_req)
 
 
 def parse(query):
@@ -49,7 +61,7 @@ def parse(query):
                 continue
         elif re.search("Hourly Range", article_desc):
             min_range_value, max_range_value = re.findall('Hourly Range<\/b>: \$(\d+)\.00-\$(\d+)\.', article_desc)[0]
-            if int(min_range_value) < MIN_RANGE_FROM or int(max_range_value) < MAX_RANGE_FROM:
+            if int(min_range_value) < MIN_RANGE_VALUE or int(max_range_value) < MAX_RANGE_VALUE:
                 print(f"skipping because hourly range)\n")
 
                 continue
